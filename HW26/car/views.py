98a1e910421e59d8d8ba3df4eb12178e1dc1f76c
Oksana <79321579+roksavsk@users.noreply.github.com>
2021-08-10 23:07:11 +0300

    Add files via upload

--- HW26/car/views.py
+++ HW26/car/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from car.models import Car
 
-# Create your views here.
+
+class CarsListView(ListView):
+    model = Car.objects.all()
+    context_object_name = 'cars'
+    template_name = 'car/list.html'
+
+    def get_queryset(self):
+        return Car.objects.all()
+
+
+class DetailCarView(DetailView):
+    model = Car
+    context_object_name = 'cars'
+    slug_url_kwarg = 'car_slug'
+    template_name = 'car/detail.html'
+
+
+def serialized_cars(request):
+    data = serializers.serialize('json', Car.objects.all())
+    return HttpResponse(request.method + '<br>' + data)
--- HW26/django_orm/urls.py
+++ HW26/django_orm/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import MainPageTemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+    path(
+        '',
+        MainPageTemplateView.as_view(),
+    ),
+    path(
+        'car/',
+        include('car.urls')
+    ),
+    path(
+        'dealer/',
+        include('dealer.urls')
+    ),
+    path(
+        'order/',
+        include('order.urls')
+    ),
 ]
